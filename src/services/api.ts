0c061527406b593b4aaf0c6970b6693e587557b3
Shantanu700 <shantanugupta13524@gmail.com>
2025-09-15 10:28:50 +0530

    feat: added backend Hit

--- src/services/api.ts
+++ src/services/api.ts
@@ -35,7 +35,13 @@ function createIssueFromBackend(data: { title: string; description: string; imag
 
 
 export async function fetchAllIssues(): Promise<Issue[]> {
-  const res = await fetch("https://0.0.0.0:8000/core/issue/");
+  const res = await fetch("https://db99d5e95bd1.ngrok-free.app/core/issue/", {
+    headers : {
+      "ngrok-skip-browser-warning" : "69420",
+    }
+  }
+    
+  );
   const backendRaw = await res.json();
 
   const backendIssues = backendRaw.map((item: any) =>
